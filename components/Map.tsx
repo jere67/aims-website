04b7667d08b2beec74e41e80ab3ac96b4eeb9e12
jere67 <jeremymoon567@gmail.com>
2025-05-01 22:02:20 -0700

    complete where we come from

--- components/Map.tsx
+++ components/Map.tsx
@@ -18,30 +18,122 @@ LoadingComponent.displayName = 'LoadingComponent'
 
 const mapData = [
   {
-    start: { lat: 64.2008, lng: -149.4937, country: "Alaska, USA" },
-    end: { lat: 34.0522, lng: -118.2437, country: "Los Angeles, USA" },
+    start: { lat: 37.8044, lng: -122.2711, country: "Oakland, CA, USA" },
+    end: { lat: 49.2827, lng: -123.1207, country: "Vancouver, Canada" },
   },
   {
-    start: { lat: 64.2008, lng: -149.4937, country: "Alaska, USA" },
-    end: { lat: -15.7975, lng: -47.8919, country: "Brasilia, Brazil" },
+    start: { lat: 40.2959, lng: -74.8702, country: "Washington Crossing, PA, USA" },
+    end: { lat: 37.5407, lng: -77.4360, country: "Richmond, VA, USA" },
   },
   {
-    start: { lat: -15.7975, lng: -47.8919, country: "Brasilia, Brazil" },
-    end: { lat: 38.7223, lng: -9.1393, country: "Lisbon, Portugal" },
+    start: { lat: 37.5407, lng: -77.4360, country: "Richmond, VA, USA" },
+    end: { lat: 43.1566, lng: -77.6088, country: "Rochester, NY, USA" },
   },
   {
-    start: { lat: 51.5074, lng: -0.1278, country: "London, UK" },
-    end: { lat: 28.6139, lng: 77.209, country: "Delhi, India" },
+    start: { lat: 42.3214, lng: -83.3330, country: "Garden City, MI, USA" },
+    end: { lat: 42.3314, lng: -83.0458, country: "Detroit, MI, USA" }, // Using Detroit for "Michigan, USA"
   },
   {
-    start: { lat: 28.6139, lng: 77.209, country: "Delhi, India" },
-    end: { lat: 43.1332, lng: 131.9113, country: "Vladivostok, Russia" },
+    start: { lat: 42.5767, lng: -83.3813, country: "West Bloomfield, MI, USA" },
+    end: { lat: 42.2808, lng: -83.7430, country: "Ann Arbor, MI, USA" },
   },
   {
-    start: { lat: 28.6139, lng: 77.209, country: "Delhi, India" },
-    end: { lat: -1.2921, lng: 36.8219, country: "Nairobi, Kenya" },
+    start: { lat: 42.3314, lng: -83.0458, country: "Detroit, MI, USA" },
+    end: { lat: 42.2808, lng: -83.7430, country: "Ann Arbor, MI, USA" },
   },
-]
+  {
+    start: { lat: 42.1103, lng: -88.0342, country: "Palatine, IL, USA" },
+    end: { lat: 41.4817, lng: -81.8001, country: "Lakewood, OH, USA" },
+  },
+  {
+    start: { lat: 36.1627, lng: -86.7816, country: "Nashville, TN, USA" },
+    end: { lat: 33.5207, lng: -86.8025, country: "Birmingham, AL, USA" },
+  },
+  {
+    start: { lat: 41.4817, lng: -81.8001, country: "Lakewood, OH, USA" },
+    end: { lat: 36.1627, lng: -86.7816, country: "Nashville, TN, USA" },
+  },
+  {
+    start: { lat: 37.8044, lng: -122.2711, country: "Oakland, CA, USA" },
+    end: { lat: 43.1566, lng: -77.6088, country: "Rochester, NY, USA" },
+  },
+  {
+    start: { lat: 49.2827, lng: -123.1207, country: "Vancouver, Canada" },
+    end: { lat: 37.5407, lng: -77.4360, country: "Richmond, VA, USA" },
+  },
+   {
+    start: { lat: 42.1103, lng: -88.0342, country: "Palatine, IL, USA" },
+    end: { lat: 40.2959, lng: -74.8702, country: "Washington Crossing, PA, USA" },
+  },
+   {
+    start: { lat: 42.1103, lng: -88.0342, country: "Palatine, IL, USA" },
+    end: { lat: 37.8044, lng: -122.2711, country: "Oakland, CA, USA" },
+  },
+  {
+    start: { lat: 41.4817, lng: -81.8001, country: "Lakewood, OH, USA" },
+     end: { lat: 43.1566, lng: -77.6088, country: "Rochester, NY, USA" },
+  },
+  {
+    start: { lat: 37.8044, lng: -122.2711, country: "Oakland, CA, USA" },
+    end: { lat: 36.3504, lng: 127.3845, country: "Daejeon, South Korea" },
+  },
+  {
+    start: { lat: 36.3504, lng: 127.3845, country: "Daejeon, South Korea" },
+    end: { lat: 32.0617, lng: 118.7778, country: "Jiangsu, China" },
+  },
+  {
+    start: { lat: 32.0617, lng: 118.7778, country: "Jiangsu, China" },
+    end: { lat: 25.0440, lng: 102.7053, country: "Kunming, China" },
+  },
+  {
+    start: { lat: 25.0440, lng: 102.7053, country: "Kunming, China" },
+    end: { lat: 3.1390, lng: 101.6869, country: "Kuala Lumpur, Malaysia" },
+  },
+   {
+    start: { lat: 3.1390, lng: 101.6869, country: "Kuala Lumpur, Malaysia" },
+    end: { lat: 14.5864, lng: 121.1747, country: "Antipolo, Philippines" },
+  },
+  {
+    start: { lat: 3.1390, lng: 101.6869, country: "Kuala Lumpur, Malaysia" },
+    end: { lat: 23.8103, lng: 90.4125, country: "Dhaka, Bangladesh" },
+  },
+   {
+    start: { lat: 23.8103, lng: 90.4125, country: "Dhaka, Bangladesh" },
+    end: { lat: 26.9124, lng: 75.7873, country: "Jaipur, India" },
+  },
+   {
+    start: { lat: 26.9124, lng: 75.7873, country: "Jaipur, India" },
+    end: { lat: 30.3165, lng: 78.0322, country: "Dehradun, India" },
+  },
+  {
+    start: { lat: 30.3165, lng: 78.0322, country: "Dehradun, India" },
+    end: { lat: 26.3260, lng: 43.9750, country: "Qassim, Saudi Arabia" },
+  },
+   {
+    start: { lat: 26.3260, lng: 43.9750, country: "Qassim, Saudi Arabia" },
+    end: { lat: 32.5568, lng: 35.8470, country: "Irbid, Jordan" },
+  },
+  {
+    start: { lat: 32.5568, lng: 35.8470, country: "Irbid, Jordan" },
+    end: { lat: 41.0082, lng: 28.9784, country: "Istanbul, Turkey" },
+  },
+   {
+    start: { lat: 41.0082, lng: 28.9784, country: "Istanbul, Turkey" },
+    end: { lat: 50.4501, lng: 30.5234, country: "Kyiv, Ukraine" },
+  },
+  {
+    start: { lat: 50.4501, lng: 30.5234, country: "Kyiv, Ukraine" },
+    end: { lat: 43.1566, lng: -77.6088, country: "Rochester, NY, USA" },
+  },
+  {
+    start: { lat: 41.0082, lng: 28.9784, country: "Istanbul, Turkey" },
+     end: { lat: 8.5000, lng: 4.5500, country: "Ilorin, Nigeria" },
+  },
+   {
+    start: { lat: 8.5000, lng: 4.5500, country: "Ilorin, Nigeria" },
+    end: { lat: 33.5207, lng: -86.8025, country: "Birmingham, AL, USA" },
+  },
+];
 
 const WorldMap = dynamic(() => import("@/components/ui/WorldMap"), {
   ssr: false,
